c35b529814f8104fdf92ff242914f17ff39b6c37
Cape23180909 <samilgamer@outlook.com>
2025-06-05 21:59:28 -0400

    Terminando de Main Activity del arreglo del parcial

--- app/src/main/java/edu/ucne/eddycapellan_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/eddycapellan_p1_ap2/MainActivity.kt
@@ -4,51 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.eddycapellan_p1_ap2.data.local.database.TareaDb
-import edu.ucne.eddycapellan_p1_ap2.data.local.navegation.TareaNavHost
-import edu.ucne.eddycapellan_p1_ap2.data.local.presentation.TareaListScreen
-import edu.ucne.eddycapellan_p1_ap2.data.local.presentation.TareaViewModel
-import edu.ucne.eddycapellan_p1_ap2.data.local.repository.TareaRepository
+import edu.ucne.eddycapellan_p1_ap2.presentation.navegation.TareaNavHost
 import edu.ucne.eddycapellan_p1_ap2.data.local.theme.EddyCapellan_P1_Ap2Theme
 
-class MainActivity : ComponentActivity() {
 
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
     private lateinit var tareaDb: TareaDb
-    private lateinit var tareasRepository: TareaRepository
-    private lateinit var tareasViewModel: TareaViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-         tareaDb = Room.databaseBuilder(
-             applicationContext,
-             TareaDb::class.java,
-             "Tarea.db"
-         ).fallbackToDestructiveMigrationFrom()
-             .build()
-
-        tareasRepository = TareaRepository(tareaDb.TareaDao())
-        tareasViewModel = TareaViewModel(tareasRepository)
+        tareaDb = Room.databaseBuilder(
+            applicationContext,
+            TareaDb::class.java,
+            "Tarea.db"
+        ).fallbackToDestructiveMigration()
+            .build()
 
         setContent {
             EddyCapellan_P1_Ap2Theme {
-                val navController = rememberNavController()
-                TareaNavHost(
-                    navController = navController,
-                    tareaViewModel = tareasViewModel
-                )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { paddingValues ->
+                    Box(modifier = Modifier.padding(paddingValues)) {
+                        TareaNavHost(
+                            navHostController = rememberNavController()
+                        )
+                    }
                 }
             }
         }
     }
+}
